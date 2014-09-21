ffec0d0d55f693596c72acc47b0015d4eba0814f
Evan R. Murphy <evanrmurphy@gmail.com>
2014-09-20 21:32:26 -0500

    Revert "Compile `.es6.js` files with Traceur"
    This reverts commit fe8048200c037226f7f5f97e3463083635e7f928.
    
    Can't get this Traceur loader to work. Not sure if it's an issue with
    Traceur or the loader, but going to remove this loader and try a
    different one to see.

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,4 @@ module.exports =
 
   , devServer:
       { contentBase: path.join(__dirname, 'src') }
-
-  , module:
-      { loaders:
-          [ { test: /\.es6\.js$/, loader: 'traceur-loader' } ]
-      }
   }
