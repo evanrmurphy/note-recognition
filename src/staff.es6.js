1cb28075cf38731f0130f80c976550dea73efdde
Evan R. Murphy <evanrmurphy@gmail.com>
2014-09-21 01:25:45 -0500

    Convert treble clef SVG to React component
    And render it from the staff component, not at all positioned or scaled
    properly yet.

--- src/staff.es6.js
+++ src/staff.es6.js
@@ -4,6 +4,8 @@ var ReactCreateClass = require('react/lib/ReactCompositeComponent').createClass
   , ReactDOM = require('react/lib/ReactDOM')
   , range = require('lodash.range')
 
+var TrebleClef = require('./treble-clef.es6.js')
+
 module.exports =
   ReactCreateClass
     ( { render:
@@ -13,6 +15,7 @@ module.exports =
                       , range(0, 50, 10).map
                           ((y) => rect({y: y, width: '100%', height: 1
                                        ,fill: 'black'}))
+                      , TrebleClef()
                       )
           }
       }
