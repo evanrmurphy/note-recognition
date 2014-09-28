8fddb72bc1a5219e08cf7659c9fe869076e4455d
Evan R. Murphy <evanrmurphy@gmail.com>
2014-09-28 00:27:53 -0500

    Refactor `notes.subscribe` handler

--- src/main.es6.js
+++ src/main.es6.js
@@ -13,28 +13,23 @@ var App = require('./app.es6.js')
 var notes = new Rx.BehaviorSubject
 
 notes.subscribe(_ => {
-  var otherNotes = without(Staff.notes, notes.value)
-    , guesses = new Rx.BehaviorSubject
+  var guesses = new Rx.BehaviorSubject
     , correctGuesses = guesses.skip(1).filter(g => g === notes.value)
-    , renderApp = () =>
-        ReactRenderComponent
-          ( App( { note: notes.value
-                 , onGuess: guesses.onNext.bind(guesses)
-                 , lastGuess: guesses.value
-                 , isLastGuessCorrect: guesses.value === notes.value
-                 }
-               )
-          , document.body
-          )
-
-  correctGuesses.subscribe
-    ( _ => setTimeout(() => {
-             guesses.onCompleted()
-             notes.onNext(sample(otherNotes))
-           }, 1000)
-    )
+    , renderApp = () => ReactRenderComponent
+        ( App({ note: notes.value
+              , onGuess: guesses.onNext.bind(guesses)
+              , lastGuess: guesses.value
+              , isLastGuessCorrect: guesses.value === notes.value
+              })
+        , document.body
+        )
   renderApp()
   guesses.subscribe(renderApp, null, renderApp)
+  correctGuesses.subscribe(_ =>
+    setTimeout(() => {
+       guesses.onCompleted()
+       notes.onNext(sample(without(Staff.notes, notes.value)))
+    }, 1000))
 })
 
 notes.onNext(sample(Staff.notes))
