9661cc90bd3fb8312e57ef9d777038a7b9e26b34
Evan R. Murphy <evanrmurphy@gmail.com>
2014-09-28 02:57:41 -0500

    Add `max-width: 600px` to app container
    So that it still fills up the width of screen on small devices (thanks
    to 1b9d4a9) but not on large computer screens (it looked silly).

--- src/app.es6.js
+++ src/app.es6.js
@@ -3,6 +3,8 @@
 var ReactCreateClass = require('react/lib/ReactCompositeComponent').createClass
   , ReactDOM = require('react/lib/ReactDOM')
 
+require('./app.css')
+
 var Staff = require('./staff.es6.js')
   , GuessEntry = require('./guess-entry.es6.js')
 
@@ -13,7 +15,7 @@ module.exports =
             var {note, guess, onGuess, isGuessCorrect} = this.props
               , {div} = ReactDOM
 
-            return div( {}
+            return div( {className: 'App'}
                       , Staff({note})
                       , GuessEntry({guess, onGuess, isGuessCorrect})
                       )
