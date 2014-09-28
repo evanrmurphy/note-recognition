ff632487a392aa67f4db18eb7d37298638679bbd
Evan R. Murphy <evanrmurphy@gmail.com>
2014-09-27 22:04:14 -0500

    Add delay after correct guess before next note
    Includes significant rearranging of Rx logic. Allowed for deleting
    `last2Notes` observable and related code.

--- src/main.es6.js
+++ src/main.es6.js
@@ -11,25 +11,34 @@ var App = require('./app.es6.js')
   , Staff = require('./staff.es6.js')
 
 var notes = new Rx.BehaviorSubject
-  , guesses = new Rx.BehaviorSubject
-  , correctGuesses = guesses.filter(g => g === notes.value)
-  , last2Notes = notes.scan([], ([_, last], current) => [last, current])
-  , lastNote
+  , guesses
+  , correctGuesses
   , otherNotes
+  , renderApp = () => {
+      ReactRenderComponent
+        ( App( { note: notes.value
+               , onGuess: guesses.onNext.bind(guesses)
+               , lastGuess: guesses.value
+               , isLastGuessCorrect: guesses.value === notes.value
+               }
+             )
+        , document.body
+        )
+    }
 
-correctGuesses.subscribe(_ => notes.onNext(sample(otherNotes)))
-
-notes.merge(guesses).subscribe
-  (() => ReactRenderComponent
-           ( App( { note: notes.value
-                  , onGuess: guesses.onNext.bind(guesses)
-                  , lastGuess: guesses.value
-                  , isLastGuessCorrect: guesses.value === lastNote
-                  }
-                )
-           , document.body
-           ))
-last2Notes.subscribe(([last, _]) => lastNote = last)
-notes.subscribe(_ => otherNotes = without(Staff.notes, notes.value))
+notes.subscribe(_ => {
+  otherNotes = without(Staff.notes, notes.value)
+  guesses = new Rx.BehaviorSubject
+  correctGuesses = guesses.skip(1).filter(g => g === notes.value)
+  correctGuesses.subscribe
+    ( _ => {
+        setTimeout(() => {
+          guesses.onCompleted()
+          notes.onNext(sample(otherNotes))
+        }, 1000)
+      }
+    )
+  notes.merge(guesses).subscribe(renderApp, null, renderApp)
+})
 
 notes.onNext(sample(Staff.notes))
