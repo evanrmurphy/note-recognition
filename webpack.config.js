968855ff3ea4089575f99c78e2e620770f2714c2
Evan R. Murphy <evanrmurphy@gmail.com>
2014-10-03 09:10:38 -0500

    Configure webpack to use rework-loader
    Also remove `css-loader` which we no longer need.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 'use strict'
 
 var path = require('path')
+var reworkLoader = require('rework-loader');
+var reworkVars = require('rework-vars');
+var reworkCalc = require('rework-calc');
+var reworkCustomMedia = require('rework-custom-media');
+var varMap = reworkLoader.makeVarMap('src/index.css');
 
 module.exports =
   { entry: path.join(__dirname, 'src/main.es6.js')
@@ -16,7 +21,18 @@ module.exports =
   , module:
       { loaders:
           [ {test: /\.es6\.js$/, loader: 'webpack-traceur'}
-          , {test: /\.css$/, loader: 'style!css'}
+          , {test: /\.css$/, loader: 'style-loader!rework-loader'}
           ]
+      }
+
+  , rework:
+      { use: 
+        [ reworkLoader.plugins.imports,
+        , reworkLoader.plugins.urls,
+        , reworkLoader.plugins.stripLocalDefs(varMap),
+        , reworkCustomMedia({map: varMap}),
+        , reworkVars({map: varMap}),
+        , reworkCalc
+        ]
       }
   }
