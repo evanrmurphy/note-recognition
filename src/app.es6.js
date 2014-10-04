1903fe286971e6f2f6a9e5ca01ff52c57a6e6a06
Evan R. Murphy <evanrmurphy@gmail.com>
2014-10-04 17:15:37 -0500

    Revert "Use webpack to load Font Awesome (not working)"
    This reverts commit 7da4f2f17914a56f5ea5fb4c7df0489163a99f7f.

--- src/app.es6.js
+++ src/app.es6.js
@@ -3,7 +3,6 @@
 var ReactCreateClass = require('react/lib/ReactCompositeComponent').createClass
   , ReactDOM = require('react/lib/ReactDOM')
 
-require('./lib/font-awesome/css/font-awesome.css')
 require('suitcss-components-grid')
 require('./index.css')
 require('./app.css')
@@ -16,12 +15,11 @@ module.exports =
     ( { render:
           function() {
             var {note, guess, onGuess, isGuessCorrect} = this.props
-              , {div, span} = ReactDOM
+              , {div} = ReactDOM
 
             return div( {className: 'App Grid-cell--center'}
                       , Staff({note})
                       , GuessEntry({guess, onGuess, isGuessCorrect})
-                      , span({className: 'fa fa-check', style: {fontSize: '20px'}})
                       )
           }
       }
--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,6 @@ module.exports =
       { loaders:
           [ {test: /\.es6\.js$/, loader: 'webpack-traceur'}
           , {test: /\.css$/, loader: 'style-loader!rework-loader'}
-          , {test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff'}
-          , {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader'}
           ]
       }
 
