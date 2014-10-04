7da4f2f17914a56f5ea5fb4c7df0489163a99f7f
Evan R. Murphy <evanrmurphy@gmail.com>
2014-10-04 17:09:49 -0500

    Use webpack to load Font Awesome (not working)
    There are many examples of this webpack config:
    
    - https://www.npmjs.org/package/font-awesome-webpack
    - https://github.com/bline/bootstrap-webpack
    - https://github.com/aaronj1335/irked/blob/master/webpack.config.js
    
    It's not working here though. The test icon added to `src/app.es6.js`
    isn't showing, and the Chrome dev console shows errors for these
    requests:
    
    GET http://localhost:8080/d95d6f5d5ab7cfefd09651800b69bd54.woffv=4.2.0
    GET http://localhost:8080/c4668ed2440df82d3fd2f8be9d31d07d.ttfv=4.2.0
    GET http://localhost:8080/65bcbc899f379216109acd0b6c494618.svgv=4.2.0

--- src/app.es6.js
+++ src/app.es6.js
@@ -3,6 +3,7 @@
 var ReactCreateClass = require('react/lib/ReactCompositeComponent').createClass
   , ReactDOM = require('react/lib/ReactDOM')
 
+require('./lib/font-awesome/css/font-awesome.css')
 require('suitcss-components-grid')
 require('./index.css')
 require('./app.css')
@@ -15,11 +16,12 @@ module.exports =
     ( { render:
           function() {
             var {note, guess, onGuess, isGuessCorrect} = this.props
-              , {div} = ReactDOM
+              , {div, span} = ReactDOM
 
             return div( {className: 'App Grid-cell--center'}
                       , Staff({note})
                       , GuessEntry({guess, onGuess, isGuessCorrect})
+                      , span({className: 'fa fa-check', style: {fontSize: '20px'}})
                       )
           }
       }
