f7d0e800d0a3d77b2d97e36095c78a7ad7679cce
Evan R. Murphy <evanrmurphy@gmail.com>
2014-10-05 02:25:19 -0500

    After incorrect guess, only show red X for 333ms
    Before we were highlighting the mistake indefinitely, until the user
    guessed again. Limiting the amount of time you see a glaring red X makes
    the game not so demoralizing.

---
+++ src/constants.es6.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports =
+  { waitOnCorrectGuess: 667
+  , waitOnIncorrectGuess: 333
+  }
--- src/index.es6.js
+++ src/index.es6.js
@@ -7,12 +7,15 @@ var React = require('react')
 
 var App = require('./app/index.es6.js')
   , Staff = require('./staff.es6.js')
+  , constants = require('./constants.es6.js')
 
 var notes = new Rx.BehaviorSubject
 
 notes.subscribe(_ => {
   var guesses = new Rx.BehaviorSubject
     , correctGuesses = guesses.skip(1).filter(g => g === notes.value)
+    , incorrectGuesses = guesses.skip(1).filter(g => g !== notes.value)
+    , guessedAt
 
   var renderApp = () => {
     var note = notes.value
@@ -21,18 +24,22 @@ notes.subscribe(_ => {
       , onGuess = guess => {
           if (!isGuessCorrect) guesses.onNext(guess)
         }
-    React.renderComponent( App({note, guess, isGuessCorrect, onGuess})
+    React.renderComponent( App({note, guess, guessedAt, isGuessCorrect
+                               ,onGuess})
                          , document.body
                          )
   }
 
   renderApp()
+  guesses.subscribe(() => guessedAt = new Date)
   guesses.subscribe(renderApp, null, renderApp)
   correctGuesses.subscribe(_ =>
     setTimeout(() => {
-       guesses.onCompleted()
-       notes.onNext(sample(without(Staff.notes, notes.value)))
-    }, 667))
+      guesses.onCompleted()
+      notes.onNext(sample(without(Staff.notes, notes.value)))
+    }, constants.waitOnCorrectGuess))
+  incorrectGuesses.subscribe(_ =>
+    setTimeout(() => renderApp(), constants.waitOnIncorrectGuess))
 })
 
 notes.onNext(sample(Staff.notes))
